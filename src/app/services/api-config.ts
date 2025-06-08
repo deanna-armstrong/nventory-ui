9885f54bdbc63d4f89246e891788bb9015f5d641
deanna-armstrong <152566979+deanna-armstrong@users.noreply.github.com>
2025-06-08 16:26:37 -0400

    api update

--- src/app/services/api-config.ts
+++ src/app/services/api-config.ts
@@ -1,4 +1,4 @@
 export const API_BASE_URL =
   window.location.hostname.includes('localhost')
-    ? 'http://localhost:3000'
-    : 'https://nventory-api.onrender.com';
+    ? 'http://localhost:3000/api'
+    : 'https://nventory-api.onrender.com/api';
